0aefde182a1b04f48db363ca4d6ab12ea6962231
Lautaro Hickmann <lautyhick@hotmail.com>
2021-10-05 20:41:29 +0200

    Added test phase and improved logging

--- Catcher/train_classic_catcher.py
+++ Catcher/train_classic_catcher.py
@@ -70,7 +70,7 @@ def process_state(state):
 def test_phase(env, model, epsilon, n_actions, steps_target_per_episode, repetitions):
     actions = [97, None, 100]
     wandb.define_metric("repetition")
-    wandb.define_metric("episode_reward", step_metric="repetition")
+    wandb.define_metric("test/*", step_metric="repetition")
 
     for r in range(repetitions):
         env.reset_game()
@@ -96,10 +96,11 @@ def test_phase(env, model, epsilon, n_actions, steps_target_per_episode, repetit
 
             if env.game_over():
                 break
-        wandb.log({'episode_reward': env.score(), "repetition": r})
+        wandb.log({'test/episode_reward': env.score(), "repetition": r})
+        print(env.score())
 
 
-def show_epopch(env, model, epsilon, n_actions, steps_target_per_episode, episode, save_dir, save=False, ):
+def show_epopch(env, model, epsilon, n_actions, steps_target_per_episode, episode, save_dir, test_table, save=False, upload=False):
     actions = [97, None, 100]
 
     env.reset_game()
@@ -139,7 +140,12 @@ def show_epopch(env, model, epsilon, n_actions, steps_target_per_episode, episod
     print("Score: ", env.score(), "\n")
     if save:
         frames[1].save(f"{save_dir}/gifs/Simplified_catcher_epsiode_{episode}_steps_{step}.gif", save_all=True, append_images=frames[2:], optimize=False, loop=0)
-        wandb.log({"video": wandb.Video(f"{save_dir}/gifs/Simplified_catcher_epsiode_{episode}_steps_{step}.gif")}, step=episode)
+    if upload:
+        # ["Episode", "Video", "steps", "score"]
+        test_table.add_data(episode, wandb.Video(f"{save_dir}/gifs/Simplified_catcher_epsiode_{episode}_steps_{step}.gif"), step, env.score())
+
+        # wandb.log({"video": wandb.Video(f"{save_dir}/gifs/Simplified_catcher_epsiode_{episode}_steps_{step}.gif"), "episode":episode})
+
 
 def get_parsed():
     parser = argparse.ArgumentParser(description="Define and run the experiment with one config")
@@ -149,9 +155,10 @@ def get_parsed():
 
     return arguments
 
+
 def main():
     physical_devices = tf.config.list_physical_devices('GPU')
-    if len(physical_devices)>0:
+    if len(physical_devices) > 0:
         tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     layers = [32, 32]  # layers = [9, 4] layers = [64] layers = [13] layers = [32, 32]
@@ -160,6 +167,7 @@ def main():
     name = f"Run-{arguments['run']}"
     project = "Catcher-Simplified"
     arg_mod = "Classic_v1"
+    type = "classic"
 
     message_name = f"{project}/{arg_mod}/{name}"
     save_dir = os.path.join("Saves", message_name)
@@ -172,11 +180,11 @@ def main():
         print("Successfully created the directory %s " % save_dir)
 
     try:
-        os.makedirs(save_dir+"/gifs")
+        os.makedirs(save_dir + "/gifs")
     except OSError:
-        print(" %s already exists or error" % (save_dir+"/gifs"))
+        print(" %s already exists or error" % (save_dir + "/gifs"))
     else:
-        print("Successfully created the directory %s " % (save_dir+"/gifs"))
+        print("Successfully created the directory %s " % (save_dir + "/gifs"))
 
     n_state = 3  # Dimension of the state vectors in CartPole
     n_actions = 3  # Number of actions in CartPole
@@ -187,7 +195,7 @@ def main():
     print(model_target.summary())
 
     gamma = 0.99
-    n_episodes = 50000
+    n_episodes = 30000
     steps_target_per_episode = 250
     # Define replay memory
     max_memory_length = 10000  # Maximum replay length
@@ -199,8 +207,9 @@ def main():
     batch_size = 32
     steps_per_update = 25  # Train the model every x steps
     steps_per_target_update = 75  # Update the target model every x steps
-    test_steps_target_per_episode = 10000
+    test_steps_target_per_episode = 1000
     test_repetitions = 100
+    grad_log_steps = 100
 
     arguments = {
         "layers": layers,
@@ -215,7 +224,9 @@ def main():
         "steps_per_update": steps_per_update,
         "steps_per_target_update": steps_per_target_update,
         "test_steps_target_per_episode": test_steps_target_per_episode,
-        "test_repetitions": test_repetitions
+        "test_repetitions": test_repetitions,
+        "grad_log_steps": grad_log_steps,
+        "type": type,
     }
 
     run = wandb.init(project=project,
@@ -224,13 +235,29 @@ def main():
                      config=arguments,
                      dir=save_dir)
 
-    wandb.alert(title="Experiment Startef", text=f"Experiment {message_name} Started", wait_duration=timedelta(seconds=1))
+    # create a Table with the same columns as above,
+    # plus confidence scores for all labels
+    columns = ["Episode", "Video", "steps", "score"]
+
+    test_table = wandb.Table(columns=columns)
+
+    # run inference on every image, assuming my_model returns the
+    # predicted label, and the ground truth labels are available
+
+    wandb.alert(title="Experiment Started", text=f"Experiment {message_name} Started", wait_duration=timedelta(seconds=1))
+
+    wandb.define_metric("training_update/step")
+    grad_names = [f"training_update/{n.name}" for n in model.trainable_variables] + ["training_update/step", "training_update/episode"]
+    wandb.define_metric("training_update/*", step_metric="repetition")
+
+    wandb.define_metric("episode")
+    wandb.define_metric("episode_reward", step_metric="episode")
 
     optimizer = tf.keras.optimizers.Adam()  # learning_rate=1e-2)
 
     game = Catcher(width=512, height=512, init_lives=3)
     p = PLE(game, display_screen=False, state_preprocessor=process_state)  # ,  force_fps=False, fps=240)
-    env = PLE(game, display_screen=True, state_preprocessor=process_state, force_fps=False, fps=2)
+    env = PLE(game, display_screen=True, state_preprocessor=process_state)  # , force_fps=False, fps=2)
 
     episode_reward_history = []
     episode_score_history = []
@@ -239,6 +266,8 @@ def main():
 
     step_count = 0
     t = trange(n_episodes, desc='Training', leave=True)
+    train_updates = 0
+    best_avg_100_score = 0
 
     for episode in t:
         episode_reward = 0
@@ -259,7 +288,9 @@ def main():
             steps_in_episode += 1
 
             # Update model
+
             if step_count % steps_per_update == 0:
+                train_updates += 1
                 # Sample a batch of interactions and update Q_function
                 training_batch = np.random.choice(replay_memory, size=batch_size)
                 grads = Q_learning_update(np.asarray([x['state'] for x in training_batch]),
@@ -268,9 +299,10 @@ def main():
                                           np.asarray([x['next_state'] for x in training_batch]),
                                           np.asarray([x['done'] for x in training_batch], dtype=np.float32),
                                           model, model_target, gamma, optimizer, n_actions)
-                grad_names = [n.name for n in model.trainable_variables]
-                grad_list = [wandb.Histogram(g) for g in grads]
-                wandb.log(dict(zip(grad_names, grad_list)), step=episode)
+
+                grad_list = [wandb.Histogram(g) for g in grads] + [train_updates, episode]
+                if train_updates % 100 == 0:
+                    wandb.log(dict(zip(grad_names, grad_list)))
 
             # Update target model
             if step_count % steps_per_target_update == 0:
@@ -279,23 +311,28 @@ def main():
             # Check if the episode is finished
             if interaction['done']:
                 break
-        if n_episodes > 100: # For making testing faster. Not triggered when running very few episodes i.e. debugging new features
-            if episode % (n_episodes // 10) == 0:
-                show_epopch(env, model_target, epsilon, n_actions, steps_target_per_episode, episode, save_dir, True)
-        else:
-            if episode % (n_episodes // 2) == 0:
-                show_epopch(env, model_target, epsilon, n_actions, steps_target_per_episode, episode, save_dir, True)
+
+        if episode % (n_episodes // 10) == 0:
+            show_epopch(env, model_target, epsilon, n_actions, steps_target_per_episode, episode, save_dir, test_table, True, True)
 
         # Decay epsilon
         epsilon = max(epsilon * decay_epsilon, epsilon_min)
         episode_reward_history.append(episode_reward)
-        wandb.log({'episode_reward': episode_reward}, step=episode)
+        wandb.log({'episode_reward': episode_reward, "steps_in_episode": steps_in_episode, "episode": episode})
         episode_score_history.append(p.score())
         steps_per_ep_history.append(steps_in_episode)
-        avg_rewards = np.mean(episode_reward_history[-100:])
+        avg_rewards = np.mean(episode_reward_history[-10:])
 
-        avg_rewards_200 = np.mean(episode_reward_history[-200:])
-        t.set_description("Episode {:5d}/{:5d}, steps in ep {:4d}, score {:04.2f}, avg 100 rew {:04.2f}, avg 200 rew {:04.2f}".format(episode + 1, n_episodes, steps_in_episode, p.score(), avg_rewards, avg_rewards_200))
+        avg_rewards_100 = np.mean(episode_reward_history[-100:])
+        # Save model if avg score of las 100 is the best
+        if avg_rewards_100 >= best_avg_100_score:
+            wandb.run.summary["Saved_at_episode"] = episode
+            wandb.run.summary["Saved_avg_score"] = avg_rewards_100
+            wandb.run.summary["Saved_avg"] = p.score()
+            model_target.save_weights(save_dir + "/saved_weights/target_weights")
+            best_avg_100_score = avg_rewards_100
+
+        t.set_description("Episode {:5d}/{:5d}, steps in ep {:4d}, score {:04.2f}, avg 10 rew {:04.2f}, avg 100 rew {:04.2f}".format(episode + 1, n_episodes, steps_in_episode, p.score(), avg_rewards, avg_rewards_100))
         t.refresh()  # to show immediately the update
         # if avg_rewards_200 > (steps_target_per_episode//5 )*.95:
         #     break
@@ -310,11 +347,20 @@ def main():
     # plt.plot(steps_per_ep_history)
     # plt.show()
     # Testing
-    show_epopch(env, model_target, epsilon, n_actions, steps_target_per_episode, episode, save_dir, True)
+    wandb.run.summary["Final_total_interaction_steps"] = step_count
+    wandb.run.summary["Final_train_updates"] = train_updates
+
+    show_epopch(env, model_target, epsilon, n_actions, steps_target_per_episode, episode, save_dir, test_table, True, True)
+
+    model_target.load_weights(save_dir + "/saved_weights/target_weights")
+
+    show_epopch(env, model_target, epsilon, n_actions, steps_target_per_episode, episode + 1, save_dir, test_table, True, True)
+    show_epopch(env, model_target, epsilon, n_actions, test_steps_target_per_episode, episode + 2, save_dir, test_table, True, True)
+
+    wandb.log({"video_table": test_table})
 
     test_phase(p, model_target, epsilon, n_actions, test_steps_target_per_episode, test_repetitions)
 
-    model_target.save_weights(save_dir + "/target_weights")
     wandb.alert(title="Experiment finished", text=f"Experiment {message_name} ended", wait_duration=timedelta(seconds=1))
 
     run.finish()
