8aaa401167333a25286e17567d46ee87fdc011a1
Lautaro Hickmann <lautyhick@hotmail.com>
2021-10-06 10:26:12 +0200

    Added quantum training to train_catcher and added Dummy launcher to setup pycharm parrallel runs easier.

--- Catcher/train_catcher.py
+++ Catcher/train_catcher.py
@@ -22,7 +22,7 @@ import wandb
 
 
 @tf.function
-def Q_learning_update(states, actions, rewards, next_states, done, model, model_target, gamma, optimizer, n_actions):
+def Q_learning_update_classic(states, actions, rewards, next_states, done, model, model_target, gamma, optimizer, n_actions):
     states = tf.convert_to_tensor(states)
     actions = tf.convert_to_tensor(actions)
     rewards = tf.convert_to_tensor(rewards)
@@ -46,6 +46,155 @@ def Q_learning_update(states, actions, rewards, next_states, done, model, model_
     grads = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
     return grads
+
+@tf.function
+def Q_learning_update_quantum(states, actions, rewards, next_states, done, model, model_target, gamma, n_actions, optimizer_in, optimizer_var, optimizer_out, w_in, w_var, w_out):
+    states = tf.convert_to_tensor(states)
+    actions = tf.convert_to_tensor(actions)
+    rewards = tf.convert_to_tensor(rewards)
+    next_states = tf.convert_to_tensor(next_states)
+    done = tf.convert_to_tensor(done)
+
+    # Compute their target q_values and the masks on sampled actions
+    future_rewards = model_target([next_states])
+    target_q_values = rewards + (gamma * tf.reduce_max(future_rewards, axis=1)
+                                 * (1.0 - done))
+    masks = tf.one_hot(actions, n_actions)
+
+    # Train the model on the states and target Q-values
+    with tf.GradientTape() as tape:
+        tape.watch(model.trainable_variables)
+        q_values = model([states])
+        q_values_masked = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+
+        loss = tf.keras.losses.Huber()(target_q_values, q_values_masked)  # Original
+
+    # Backpropagation
+    grads = tape.gradient(loss, model.trainable_variables)
+
+    for optimizer, w in zip([optimizer_in, optimizer_var, optimizer_out], [w_in, w_var, w_out]):
+        optimizer.apply_gradients([(grads[w], model.trainable_variables[w])])
+    return grads
+
+def generate_circuit(qubits, n_layers):
+    """Prepares a data re-uploading circuit on `qubits` with `n_layers` layers."""
+    # Number of qubits
+    n_qubits = len(qubits)
+
+    # Sympy symbols for variational angles
+    params = sympy.symbols(f'theta(0:{3 * (n_layers + 1) * n_qubits})')
+    params = np.asarray(params).reshape((n_layers + 1, n_qubits, 3))
+
+    # Sympy symbols for encoding angles
+    inputs = sympy.symbols(f'x(0:{n_qubits})' + f'(0:{n_layers})')
+    inputs = np.asarray(inputs).reshape((n_layers, n_qubits))
+
+    # Define circuit
+    circuit = cirq.Circuit()
+    for l in range(n_layers):
+        # Variational layer
+        circuit += cirq.Circuit(one_qubit_rotation(q, params[l, i]) for i, q in enumerate(qubits))
+        circuit += entangling_layer(qubits)
+        # Encoding layer
+        circuit += cirq.Circuit(cirq.rx(inputs[l, i])(q) for i, q in enumerate(qubits))
+
+    # Last varitional layer
+    circuit += cirq.Circuit(one_qubit_rotation(q, params[n_layers, i]) for i, q in enumerate(qubits))
+
+    return circuit, list(params.flat), list(inputs.flat)
+
+
+def entangling_layer(qubits):
+    """
+    Returns a layer of CZ entangling gates on `qubits` (arranged in a circular topology).
+    """
+    cz_ops = [cirq.CZ(q0, q1) for q0, q1 in zip(qubits, qubits[1:])]
+    cz_ops += ([cirq.CZ(qubits[0], qubits[-1])] if len(qubits) != 2 else [])
+    return cz_ops
+
+
+def one_qubit_rotation(qubit, symbols):
+    """
+    Returns Cirq gates that apply a rotation of the bloch sphere about the X,
+    Y and Z axis, specified by the values in `symbols`.
+    """
+    return [cirq.rx(symbols[0])(qubit),
+            cirq.ry(symbols[1])(qubit),
+            cirq.rz(symbols[2])(qubit)]
+
+
+class ReUploadingPQC(tf.keras.layers.Layer):
+    """
+    Performs the transformation (s_1, ..., s_d) -> (theta_1, ..., theta_N, lmbd[1][1]s_1, ..., lmbd[1][M]s_1,
+        ......., lmbd[d][1]s_d, ..., lmbd[d][M]s_d) for d=input_dim, N=theta_dim and M=n_layers.
+    An activation function from tf.keras.activations, specified by `activation` ('linear' by default) is
+        then applied to all lmbd[i][j]s_i.
+    All angles are finally permuted to follow the alphabetical order of their symbol names, as processed
+        by the ControlledPQC.
+    """
+
+    def __init__(self, qubits, n_layers, observables, activation="linear", name="re-uploading_PQC"):
+        super(ReUploadingPQC, self).__init__(name=name)
+        self.n_layers = n_layers
+        self.n_qubits = len(qubits)
+        circuit, theta_symbols, input_symbols = generate_circuit(qubits, n_layers)
+
+        theta_init = tf.random_uniform_initializer(minval=0.0, maxval=np.pi)
+        self.theta = tf.Variable(
+            initial_value=theta_init(shape=(1, len(theta_symbols)), dtype="float32"),
+            trainable=True, name="thetas"
+        )
+
+        lmbd_init = tf.ones(shape=(self.n_qubits * self.n_layers,))
+        self.lmbd = tf.Variable(
+            initial_value=lmbd_init, dtype="float32", trainable=True, name="lambdas"
+        )
+
+        # Define explicit symbol order.
+        symbols = [str(symb) for symb in theta_symbols + input_symbols]
+
+        self.indices = tf.constant([sorted(symbols).index(a) for a in symbols])
+        self.activation = activation
+        self.empty_circuit = tfq.convert_to_tensor([cirq.Circuit()])
+
+        self.computation_layer = tfq.layers.ControlledPQC(circuit, observables)
+
+    def call(self, inputs):
+        # inputs[0] = encoding data for the state.
+        batch_dim = tf.gather(tf.shape(inputs[0]), 0)
+        tiled_up_circuits = tf.repeat(self.empty_circuit, repeats=batch_dim)
+        tiled_up_thetas = tf.tile(self.theta, multiples=[batch_dim, 1])
+        tiled_up_inputs = tf.tile(inputs[0], multiples=[1, self.n_layers])
+        scaled_inputs = tf.einsum("i,ji->ji", self.lmbd, tiled_up_inputs)
+        squashed_inputs = tf.keras.layers.Activation(self.activation)(scaled_inputs)
+        joined_vars = tf.concat([tiled_up_thetas, squashed_inputs], axis=1)
+        joined_vars = tf.gather(joined_vars, self.indices, axis=1)
+
+        return self.computation_layer([tiled_up_circuits, joined_vars])
+
+
+class Rescaling(tf.keras.layers.Layer):
+    def __init__(self, input_dim):
+        super(Rescaling, self).__init__()
+        self.input_dim = input_dim
+        self.w = tf.Variable(
+            initial_value=tf.ones(shape=(1, input_dim)), dtype="float32",
+            trainable=True, name="obs-weights")
+
+    def call(self, inputs):
+        return tf.math.multiply((inputs + 1) / 2, tf.repeat(self.w, repeats=tf.shape(inputs)[0], axis=0))
+
+
+def generate_model_Qlearning(qubits, n_layers, n_actions, observables, target):
+    """Generates a Keras model for a data re-uploading PQC Q-function approximator."""
+
+    input_tensor = tf.keras.Input(shape=(len(qubits),), dtype=tf.dtypes.float32, name='input')
+    re_uploading_pqc = ReUploadingPQC(qubits, n_layers, observables, activation='tanh')([input_tensor])
+    process = tf.keras.Sequential([Rescaling(len(observables))], name=target * "Target" + "Q-values")
+    Q_values = process(re_uploading_pqc)
+    model = tf.keras.Model(inputs=[input_tensor], outputs=Q_values)
+
+    return model
 
 
 def generate_model(layers, n_state, n_actions):
@@ -149,7 +298,8 @@ def show_epopch(env, model, epsilon, n_actions, steps_target_per_episode, episod
 
 def get_parsed():
     parser = argparse.ArgumentParser(description="Define and run the experiment with one config")
-    parser.add_argument('--run', type=int, default=1, help="")
+    parser.add_argument('--run', type=int, help="Run iteration")
+    parser.add_argument('--Quantum', action="store_true", default=False, help="True: Quantum model, False (default) classic model")
 
     arguments = vars(parser.parse_args())
 
@@ -160,14 +310,19 @@ def main():
     physical_devices = tf.config.list_physical_devices('GPU')
     if len(physical_devices) > 0:
         tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-    layers = [32, 32]  # layers = [9, 4] layers = [64] layers = [13] layers = [32, 32]
     arguments = get_parsed()
+    Quantum = arguments["Quantum"]
 
     name = f"Run-{arguments['run']}"
     project = "Catcher-Simplified"
-    arg_mod = "Classic_v1"
-    type = "classic"
+    if Quantum:
+        arg_mod = "Quantum_v1"
+        type = "quantum"
+        global tfq
+        tfq = __import__('tensorflow_quantum', globals(), locals())
+    else:
+        arg_mod = "Classic_v1"
+        type = "classic"
 
     message_name = f"{project}/{arg_mod}/{name}"
     save_dir = os.path.join("Saves", message_name)
@@ -189,11 +344,21 @@ def main():
     n_state = 3  # Dimension of the state vectors in CartPole
     n_actions = 3  # Number of actions in CartPole
 
-    model = generate_model(layers, n_state, n_actions)
-    model_target = generate_model(layers, n_state, n_actions)
+    if Quantum:
+        n_qubits = 3  # Dimension of the state vectors in CartPole     [player x position,   fruits x position,   fruits y position]
+        n_layers = 5  # Number of layers in the PQC
+        qubits = cirq.GridQubit.rect(1, n_qubits)
+        ops = [cirq.Z(q) for q in qubits]
+        observables = [ops[0], ops[1], ops[2]]  # Z_0*Z_1 for action 0 and Z_2*Z_3 for action 1
+        model = generate_model_Qlearning(qubits, n_layers, n_actions, observables, False)
+        model_target = generate_model_Qlearning(qubits, n_layers, n_actions, observables, True)
+    else:
+        layers = [32, 32]  # layers = [9, 4] layers = [64] layers = [13] layers = [32, 32]
+        model = generate_model(layers, n_state, n_actions)
+        model_target = generate_model(layers, n_state, n_actions)
+
     model_target.set_weights(model.get_weights())
     print(model_target.summary())
-
     gamma = 0.99
     n_episodes = 30000
     steps_target_per_episode = 250
@@ -211,23 +376,44 @@ def main():
     test_repetitions = 100
     grad_log_steps = 100
 
-    arguments = {
-        "layers": layers,
-        "gamma": gamma,
-        "n_episodes": n_episodes,
-        "steps_target_per_episode": steps_target_per_episode,
-        "max_memory_length": max_memory_length,
-        "epsilon": epsilon,
-        "epsilon_min": epsilon_min,
-        "decay_epsilon": decay_epsilon,
-        "batch_size": batch_size,
-        "steps_per_update": steps_per_update,
-        "steps_per_target_update": steps_per_target_update,
-        "test_steps_target_per_episode": test_steps_target_per_episode,
-        "test_repetitions": test_repetitions,
-        "grad_log_steps": grad_log_steps,
-        "type": type,
-    }
+    if Quantum:
+        arguments = {
+            "n_qubits": n_qubits,
+            "n_layers": n_layers,
+            "observables": observables,
+            "gamma": gamma,
+            "n_episodes": n_episodes,
+            "steps_target_per_episode": steps_target_per_episode,
+            "max_memory_length": max_memory_length,
+            "epsilon": epsilon,
+            "epsilon_min": epsilon_min,
+            "decay_epsilon": decay_epsilon,
+            "batch_size": batch_size,
+            "steps_per_update": steps_per_update,
+            "steps_per_target_update": steps_per_target_update,
+            "test_steps_target_per_episode": test_steps_target_per_episode,
+            "test_repetitions": test_repetitions,
+            "grad_log_steps": grad_log_steps,
+            "type": type,
+        }
+    else:
+        arguments = {
+            "layers": layers,
+            "gamma": gamma,
+            "n_episodes": n_episodes,
+            "steps_target_per_episode": steps_target_per_episode,
+            "max_memory_length": max_memory_length,
+            "epsilon": epsilon,
+            "epsilon_min": epsilon_min,
+            "decay_epsilon": decay_epsilon,
+            "batch_size": batch_size,
+            "steps_per_update": steps_per_update,
+            "steps_per_target_update": steps_per_target_update,
+            "test_steps_target_per_episode": test_steps_target_per_episode,
+            "test_repetitions": test_repetitions,
+            "grad_log_steps": grad_log_steps,
+            "type": type,
+        }
 
     run = wandb.init(project=project,
                      group=arg_mod,
@@ -252,8 +438,14 @@ def main():
 
     wandb.define_metric("episode")
     wandb.define_metric("episode_reward", step_metric="episode")
-
-    optimizer = tf.keras.optimizers.Adam()  # learning_rate=1e-2)
+    if Quantum:
+        optimizer_in = tf.keras.optimizers.Adam(learning_rate=0.001, amsgrad=True)
+        optimizer_var = tf.keras.optimizers.Adam(learning_rate=0.001, amsgrad=True)
+        optimizer_out = tf.keras.optimizers.Adam(learning_rate=0.1, amsgrad=True)
+        # Assign the model parameters to each optimizer
+        w_in, w_var, w_out = 1, 0, 2
+    else:
+        optimizer = tf.keras.optimizers.Adam()  # learning_rate=1e-2)
 
     game = Catcher(width=512, height=512, init_lives=3)
     p = PLE(game, display_screen=False, state_preprocessor=process_state)  # ,  force_fps=False, fps=240)
@@ -293,12 +485,21 @@ def main():
                 train_updates += 1
                 # Sample a batch of interactions and update Q_function
                 training_batch = np.random.choice(replay_memory, size=batch_size)
-                grads = Q_learning_update(np.asarray([x['state'] for x in training_batch]),
-                                          np.asarray([x['action'] for x in training_batch]),
-                                          np.asarray([x['reward'] for x in training_batch], dtype=np.float32),
-                                          np.asarray([x['next_state'] for x in training_batch]),
-                                          np.asarray([x['done'] for x in training_batch], dtype=np.float32),
-                                          model, model_target, gamma, optimizer, n_actions)
+                if Quantum:
+                    grads = Q_learning_update_quantum(np.asarray([x['state'] for x in training_batch]),
+                                  np.asarray([x['action'] for x in training_batch]),
+                                  np.asarray([x['reward'] for x in training_batch], dtype=np.float32),
+                                  np.asarray([x['next_state'] for x in training_batch]),
+                                  np.asarray([x['done'] for x in training_batch], dtype=np.float32),
+                                  model, model_target, gamma, n_actions, optimizer_in, optimizer_var, optimizer_out, w_in, w_var, w_out)
+
+                else:
+                    grads = Q_learning_update_classic(np.asarray([x['state'] for x in training_batch]),
+                                                      np.asarray([x['action'] for x in training_batch]),
+                                                      np.asarray([x['reward'] for x in training_batch], dtype=np.float32),
+                                                      np.asarray([x['next_state'] for x in training_batch]),
+                                                      np.asarray([x['done'] for x in training_batch], dtype=np.float32),
+                                                      model, model_target, gamma, optimizer, n_actions)
 
                 grad_list = [wandb.Histogram(g) for g in grads] + [train_updates, episode]
                 if train_updates % 100 == 0:
@@ -319,8 +520,6 @@ def main():
         epsilon = max(epsilon * decay_epsilon, epsilon_min)
         episode_reward_history.append(episode_reward)
         wandb.log({'episode_reward': episode_reward, "steps_in_episode": steps_in_episode, "episode": episode})
-        episode_score_history.append(p.score())
-        steps_per_ep_history.append(steps_in_episode)
         avg_rewards = np.mean(episode_reward_history[-10:])
 
         avg_rewards_100 = np.mean(episode_reward_history[-100:])
